3eeb5701042375869321e0548c988cfa6bedffb1
tobx <net@tobx.de>
2021-04-25 17:34:11 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let hover = true;
+</script>
+
+<main>
+  <p>This box should be green:</p>
+  <div
+    class="{$$restProps.class} w-40 h-40 bg-red-500"
+    class:bg-green-500={hover}
+  />
+</main>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mode: "jit",
+  purge: {
+    content: ["./src/**/*.svelte"],
+  },
+};
