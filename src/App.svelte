9d6b6b9de438c31d5179c46a67d1fc0a37cd9809
tobx <net@tobx.de>
2021-04-25 18:11:04 +0200

    Minimize issue

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,11 @@
 </script>
 
 <main>
-  <p>This box should be green:</p>
   <div
-    class="{$$restProps.class} w-40 h-40 bg-red-500"
-    class:bg-green-500={hover}
-  />
+    class:text-green-500={hover}
+  >
+    This text should be green.
+  </div>
 </main>
 
 <style global lang="postcss">
